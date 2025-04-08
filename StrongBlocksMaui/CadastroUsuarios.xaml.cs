b2a466aa9760fec85bc943aa9b5b339798690655
Gabrielbielvl <gabrielbirges5@gmail.com>
2025-04-08 20:26:48 -0300

    criando usuario

---
+++ StrongBlocksMaui/CadastroUsuarios.xaml.cs
@@ -0,0 +1,9 @@
+namespace StrongBlocksMaui;
+
+public partial class CadastroUsuarios : ContentPage
+{
+	public CadastroUsuarios()
+	{
+		InitializeComponent();
+	}
+}
---
+++ StrongBlocksMaui/ListagemUsuarios.xaml.cs
@@ -0,0 +1,9 @@
+namespace StrongBlocksMaui;
+
+public partial class ListagemUsuarios : ContentPage
+{
+	public ListagemUsuarios()
+	{
+		InitializeComponent();
+	}
+}
