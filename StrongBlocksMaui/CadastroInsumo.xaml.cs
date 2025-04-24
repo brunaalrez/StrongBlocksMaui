851b581827cf3a1ebc16cfd4527e51564ffb9362
Frediney <Fredineycd@hotmail.com>
2025-04-23 21:50:35 -0300

    BD_insumo

--- StrongBlocksMaui/CadastroInsumo.xaml.cs
+++ StrongBlocksMaui/CadastroInsumo.xaml.cs
@@ -1,9 +1,24 @@
-namespace StrongBlocksMaui;
-
-public partial class CadastroInsumo : ContentPage
+namespace StrongBlocksMaui
 {
-	public CadastroInsumo()
-	{
-		InitializeComponent();
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Produto p = new Produto();
+            Lista.ItemsSource = p.BuscaTodos();
+        }
+
+        private void Salvar_Clicked(object sender, EventArgs e)
+        {
+            Produto p = new Produto();
+            p.tipo_insumo = EntryInsumo.Text;
+            p.quantidade = int.Parse(EntryInsumo.Text);
+            p.Insere();
+
+            Lista.ItemsSource = null;
+            Lista.ItemsSource = p.BuscaTodos();
+        }
+    }
 }
--- StrongBlocksMaui/Estoque.cs
+++ StrongBlocksMaui/Estoque.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StrongBlocksMaui
 {
-    internal class Estoque
+    internal class Produto
     {
+        public int id { get; set; }
+        public string tipo_insumo { get; set; }
+        public int quantidade { get; set; }
+        public string fornecedor { get; set; }
+        public string tipo_produto { get; set; }
+        public DateTime data { get; set; }
+
+        Conexao conexao { get; set; }
+
+        public Produto()
+        {
+            conexao = new Conexao();
+        }
+        public void Insere()
+        {
+            string query = $"INSERT INTO produtos (nome, preco) VALUES('{tipo_insumo}','{quantidade}', {data}, '{fornecedor}', '{tipo_produto}');";
+            conexao.ExecutaComando(query);
+            Console.WriteLine("Produto inserido com sucesso");
+        }
+
+        public List<Produto> BuscaTodos()
+        {
+            DataTable dt = conexao.ExecutaSelect("SELECT * FROM produtos;");
+            List<Produto> lista = new List<Produto>();
+
+            foreach (DataRow linha in dt.Rows)
+            {
+                Produto p = new Produto();
+                p.id = int.Parse(linha["id"].ToString());
+                p.tipo_insumo = linha["tipo_de_insumo"].ToString();
+                p.quantidade = int.Parse(linha["quantidade"].ToString());
+                p.fornecedor = linha["fornecedor"].ToString();
+                p.tipo_produto = linha["tipo_de_produto"].ToString();
+                p.data = DateTime.Parse(linha["data"].ToString());
+                lista.Add(p);
+            }
+
+            return lista;
+        }
     }
 }
