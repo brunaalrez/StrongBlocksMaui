2dcfd4198f7466d9243b2edf19ed7645046a94f3
Frediney <Fredineycd@hotmail.com>
2025-05-19 15:10:57 -0300

    remoção do prestador da tela de cadastro de insumos

--- StrongBlocksMaui/Produto.cs
+++ StrongBlocksMaui/Produto.cs
@@ -86,7 +86,10 @@ namespace StrongBlocksMaui
             }
 
             return lista;
+
+
+
         }
     }
-    
+
 }
---
+++ StrongBlocksMaui/RemoverInsumos.xaml.cs
@@ -0,0 +1,31 @@
+namespace StrongBlocksMaui;
+
+public partial class RemoverInsumo : ContentPage
+{
+    private List<Produto> insumos = new();
+
+    public RemoverInsumo()
+    {
+              CarregarInsumos();
+    }
+
+    private void CarregarInsumos()
+    {
+        
+    }
+
+    private void PickerInsumos_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void Remover_Clicked(object sender, EventArgs e)
+    {
+
+
+        
+
+       
+
+    }
+}
